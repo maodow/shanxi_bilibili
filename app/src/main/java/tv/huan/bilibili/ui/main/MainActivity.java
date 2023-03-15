46376df6d34d23c13aa3c784d13c0dbb4b5c8110
zhanghang <zhanghang@huan.tv>
2023-03-15 18:24:56 +0800

    关闭：模板19大屏播放器功能

--- app/src/main/java/tv/huan/bilibili/ui/main/MainActivity.java
+++ app/src/main/java/tv/huan/bilibili/ui/main/MainActivity.java
@@ -142,8 +142,8 @@ public class MainActivity extends BaseActivity<MainView, MainPresenter> implemen
     @Override
     public void startFullPlayer() {
         stopFullPlayer();
-        PlayerView playerView = findViewById(R.id.main_player);
-        playerView.start("http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&amp;bizid=1023&amp;hy=SH&amp;fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400");
+//        PlayerView playerView = findViewById(R.id.main_player);
+//        playerView.start("http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&amp;bizid=1023&amp;hy=SH&amp;fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400");
     }
 
     @Override
